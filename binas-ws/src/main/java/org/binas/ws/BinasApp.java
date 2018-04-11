afde1fecd6983c0ce37ff3d0fbd72caa6aae58a6
asgcfigueiredo <alexandra.figueiredo@tecnico.ulisboa.pt>
2018-04-11 16:57:59 +0100

    Update BinasApp

--- binas-ws/src/main/java/org/binas/ws/BinasApp.java
+++ binas-ws/src/main/java/org/binas/ws/BinasApp.java
@@ -1,14 +1,44 @@
 package org.binas.ws;
 
+import org.binas.domain.BinasManager;
+
 public class BinasApp {
 
 	public static void main(String[] args) throws Exception {
 		System.out.println(BinasApp.class.getSimpleName() + " running");
-		// TODO
 		
-		/* Start we service 
-		 * Criar 2 classes EndPointManager e PortImpl
-		 */
+			// Check arguments
+			if (args.length < 2) {
+				System.err.println("Argument(s) missing!");
+				System.err.println("Usage: java " +  BinasApp.class.getName() + "wsName wsURL OR wsName wsURL uddiURL");
+				return;
+			}
+			String wsName = args[0];
+			String wsURL = args[1];
+			String uddiURL;
+			BinasEndpointManager endpoint;
+			
+			// handle UDDI arguments
+			if (args.length == 3) {
+				uddiURL = args[2];
+				endpoint = new BinasEndpointManager(uddiURL, wsName, wsURL);
+			}
+			else {
+				endpoint = new BinasEndpointManager(wsName, wsURL);
+			}
+			
+			//BinasManager.getInstance().setId(wsName);
+
+			System.out.println(BinasApp.class.getSimpleName() + " running");
+
+			// TODO start Web Service
+			 try {
+			endpoint.start();
+			endpoint.awaitConnections();
+			} finally {
+			endpoint.stop();
+			}
+		
 	}
 
 }
