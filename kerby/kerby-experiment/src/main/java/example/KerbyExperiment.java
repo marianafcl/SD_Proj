9cbf7717b29ba57aa434b208e199af110b84edae
Mariana Loureiro <mfc.loureiro@sapo.pt>
2018-05-08 09:23:05 +0100

    kerby experiment finished

--- kerby/kerby-experiment/src/main/java/example/KerbyExperiment.java
+++ kerby/kerby-experiment/src/main/java/example/KerbyExperiment.java
@@ -6,6 +6,7 @@ import java.util.Properties;
 import java.util.Random;
 import java.util.Date;
 import java.security.Key;
+import pt.ulisboa.tecnico.sdis.kerby.KerbyException;
 import pt.ulisboa.tecnico.sdis.kerby.RequestTime;
 import pt.ulisboa.tecnico.sdis.kerby.RequestTimeView;
 import pt.ulisboa.tecnico.sdis.kerby.CipheredView;
@@ -67,9 +68,9 @@ public class KerbyExperiment {
         String email = "alice@CXX.binas.org";
         String client_password = "Zd8hqDu23t"; /*get this argument in binas-ws-cli methods along with email*/
         String url = "http://localhost:8888/kerby"; /*constant*/
-        
-        int duration = 60; /*Cyclical? Short session.*/
-        long nounce = new Random().nextLong(); /*Is random ok? Based on password?*/
+        int duration = 120;
+        long nounce = new Random().nextLong();
+        Date date = new Date();
         
         
         Key kc = SecurityHelper.generateKeyFromPassword(client_password);
@@ -88,16 +89,16 @@ public class KerbyExperiment {
 		
 		
 		SessionKey sessionkey = new SessionKey(cipheredSessionKey, kc);
+		if(!(nounce == sessionkey.getNounce())) {
+			throw new KerbyException();
+		}
 		
 		System.out.print("Client's SessionKey (KCS included): "); System.out.println(sessionkey);
-		
-		Auth aa = new Auth(email, new Date()); /*??????????????????*/
-        AuthView a = aa.authBuild(email, new Date()); /*Timestamp?*/
         
         
-        CipheredView cipheredAuth = SecurityHelper.cipher(AuthView.class, a, sessionkey.getKeyXY());
+        CipheredView cipheredAuth = (new Auth(email, date)).cipher(sessionkey.getKeyXY());
         
-        /*handler sends ticket+auth views (ciphered ticket and cv)*/
+        /*handler sends ticket+auth views (ciphered ticket and cv) + request*/
         
         System.out.println();
 
@@ -108,7 +109,6 @@ public class KerbyExperiment {
 		// server-side code experiments
         System.out.println("Experiment with Kerberos server-side processing");
         
-        /*handler receives ciphered ticket+arth, gets individual ciphered views per client example (?)*/
         
         String server_password = "MTbvC3"; /*constant*/
         
@@ -121,6 +121,7 @@ public class KerbyExperiment {
         Ticket ticket = new Ticket(cipheredTicket, ks);
         ticket.validate();
         
+        
         System.out.print("Server's Ticket (KCS included): "); System.out.println(ticket);
         
         
@@ -128,12 +129,33 @@ public class KerbyExperiment {
         Auth auth = new Auth(cipheredAuth, kcs);
         auth.validate();
         
+        Date now = new Date();
+        if(!ticket.getX().equals(auth.getX()) || now.before(ticket.getTime1()) || now.after(ticket.getTime2())) {
+        	throw new KerbyException();
+        }
+        
         System.out.print("Server's Client's Auth: {"); System.out.print(auth.getX()); System.out.print("; "); System.out.print(auth.getTimeRequest()); System.out.println("}");
         
+   
+        CipheredView cipheredTimeRequest = (new RequestTime(auth.getTimeRequest())).cipher(kcs);
         
-        RequestTime rt = new RequestTime(auth.getTimeRequest()); /*?*/
+        /*handler sends timerequest view (cipheredTimeRequest) + response*/
         
-        /*handler responds with RequestTime? No View? What's it for?*/
+        System.out.println();
+        
+        
+        
+        
+        
+        // client-side code experiments
+        System.out.println("Experiment with Kerberos client-side processing");
+        
+        RequestTime tr = new RequestTime(cipheredTimeRequest, sessionkey.getKeyXY());
+        if(!date.equals(tr.getTimeRequest())) {
+        	throw new KerbyException();
+        }
+        
+        System.out.print("Client's Received Time Request: "); System.out.println(tr);
         
         System.out.println();
 		
